ae078d574b97baab35d8d9f6245c93fcfce9ce62
venkateswarluc <85680049+venkateswarluc@users.noreply.github.com>
2021-06-18 16:19:45 +0530

    Update test_AddNum.py

--- test_AddNum.py
+++ test_AddNum.py
@@ -6,6 +6,9 @@ class TestAddNum(unittest.TestCase):
   
   def test_add(self):
     self.assertEqual(calc.add(10,5),15)
+    self.assertEqual(calc.add(10,10),20)
+    self.assertEqual(calc.add(10,25),35)
+    self.assertEqual(calc.add(10,50),60)
     
     
     
