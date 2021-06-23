917c11f70c00d42668f686fda5144737cf153c7c
venkateswarluc <85680049+venkateswarluc@users.noreply.github.com>
2021-06-23 10:44:08 +0530

    Update test_AddNum.py

--- test_AddNum.py
+++ test_AddNum.py
@@ -1,16 +1,14 @@
 import unittest
 import AddNum
 
-
 class TestCalc(unittest.TestCase):
-
     def test_add(self):
         self.assertEqual(AddNum.add(10, 5), 15)
         self.assertEqual(AddNum.add(-1, 1), 0)
         self.assertEqual(AddNum.add(-1, -1), -2)
+  
     
-    
-    
+  
     
 if __name__ == '__main__':
     unittest.main()
