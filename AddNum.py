8aca5ed9ee3b22ef199f43c5db18042c803ecf53
venkateswarluc <85680049+venkateswarluc@users.noreply.github.com>
2021-06-23 10:58:19 +0530

    Update AddNum.py

--- AddNum.py
+++ AddNum.py
@@ -1,8 +1,3 @@
-x = 10
-y = 5
-
-# Add two numbers
-add = x + y
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(x, y, add))
+def add(x, y):
+    """Add Function"""
+    return x + y
